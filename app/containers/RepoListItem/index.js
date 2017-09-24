66a9a49207c5ca3f5715122256605adf5fa5ecdb
Mauro Delazeri <maurodelazeri@gmail.com>
2017-09-23 22:08:48 -0400

    first commit

--- app/containers/RepoListItem/index.js
+++ app/containers/RepoListItem/index.js
@@ -4,23 +4,24 @@
  * Lists the name and the issue count of a repository
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { FormattedNumber } from 'react-intl';
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { FormattedNumber } from "react-intl";
 
-import { makeSelectCurrentUser } from 'containers/App/selectors';
-import ListItem from 'components/ListItem';
-import IssueIcon from './IssueIcon';
-import IssueLink from './IssueLink';
-import RepoLink from './RepoLink';
-import Wrapper from './Wrapper';
+import { makeSelectCurrentUser } from "containers/App/selectors";
+import ListItem from "components/ListItem";
+import IssueIcon from "./IssueIcon";
+import IssueLink from "./IssueLink";
+import RepoLink from "./RepoLink";
+import Wrapper from "./Wrapper";
 
-export class RepoListItem extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+export class RepoListItem extends React.PureComponent {
+  // eslint-disable-line react/prefer-stateless-function
   render() {
     const item = this.props.item;
-    let nameprefix = '';
+    let nameprefix = "";
 
     // If the repository is owned by a different person than we got the data for
     // it's a fork and we should show the name of the owner
@@ -42,17 +43,17 @@ export class RepoListItem extends React.PureComponent { // eslint-disable-line r
     );
 
     // Render the content into a list item
-    return (
-      <ListItem key={`repo-list-item-${item.full_name}`} item={content} />
-    );
+    return <ListItem key={`repo-list-item-${item.full_name}`} item={content} />;
   }
 }
 
 RepoListItem.propTypes = {
   item: PropTypes.object,
-  currentUser: PropTypes.string,
+  currentUser: PropTypes.string
 };
 
-export default connect(createStructuredSelector({
-  currentUser: makeSelectCurrentUser(),
-}))(RepoListItem);
+export default connect(
+  createStructuredSelector({
+    currentUser: makeSelectCurrentUser()
+  })
+)(RepoListItem);
